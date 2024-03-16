8e39f9a7aee749ccb897ff524b0d117e8388229b
Marie Katrine Ekeberg <mke@themkat.net>
2024-03-16 16:30:59 +0100

    Add octals and experiment with rendering

--- main.lua
+++ main.lua
@@ -1,18 +1,17 @@
--- TODO: support a single number the user can tweak on screen using the crank
 -- TODO: support doing various operations on at least two numbers? 
 
-
--- TODO: should we maybe have a current input type? should this affect what results are shown in any way?
 -- TODO: what is the best way to handle the current selection?
---
 
 -- TODO: handling signed vs unsigned numbers?
+
 import "CoreLibs/crank"
+import "CoreLibs/graphics"
 
 local inputType = "HEX"
--- TODO: what is the best way to handle the current input? Number? String?
 local currentInput = 10
 local SETTINGS = {}
+-- TODO: workaround until all settings show.
+SETTINGS.showDecimal = true
 
 -- Initialize various settings like menu items
 local function init()
@@ -23,6 +22,10 @@ local function init()
    systemMenu:addCheckmarkMenuItem("Show hexadecimal result", false, function(value)
                                       SETTINGS.showHex = value
    end)
+   systemMenu:addCheckmarkMenuItem("Show octal result", false, function(value)
+                                      SETTINGS.showOctal = value
+   end)
+   -- TODO: why aren't more showing after the third one?
    systemMenu:addCheckmarkMenuItem("Show decimal result", false, function(value)
                                       SETTINGS.showDecimal = value
    end)
@@ -31,23 +34,41 @@ end
 init()
 
 -- utility function for converting to binary string
-function decimalToBinary(number)
-   -- TODO: have some sort of logic to print out a relevant number of bits?
+local function decimalToBinary(number)
+   -- assuming 32 bits integers as specified by playdate docs
    local result = ""
    local currentNum = number
-   while currentNum > 0 do
+   local iterations = 32
+   while iterations > 0 do
+      if iterations % 4 == 0 then
+         result = " " .. result
+      end
+
       if 1 == currentNum & 1 then
          result = "1" .. result
       else
          result = "0" .. result
       end
-      
+
       currentNum = currentNum >> 1
+      iterations -= 1
    end
 
    return result
 end
 
+-- utility function for converting to an octal string
+local function decimalToOctal(number)
+   local result = ""
+   local currentNum = number
+   while currentNum > 0 do
+      local nextDigit = currentNum & 7
+      result = nextDigit .. result
+      currentNum = currentNum >> 3
+   end
+
+   return result
+end
 
 function playdate.BButtonDown()
    -- TODO: do the best way of cycling input specificiers
@@ -60,39 +81,49 @@ function playdate.update()
 
    -- TODO: could we maybe show some usage hints? Any actions we could assign to a and b?
    --       maybe one could be switch input format?
-   
+
    -- TODO: clean up into util functions
    local crankMovement = playdate.getCrankTicks(8)
    currentInput += crankMovement
    currentInput = math.max(0, currentInput)
-   
 
+   -- TODO: maybe the
    local formatted
    if "HEX" == inputType then
       formatted = string.format("%x", currentInput)
    elseif "BIN" == inputType then
       formatted = decimalToBinary(currentInput)
+   elseif "OCT" == inputType then
+      formatted = decimalToOctal(currentInput)
    else
       formatted = string.format("%d", currentInput)
    end
-   -- TODO: better placement of the result string (if it grows, part of it gets swallowed up by the right side of the screen)
-   playdate.graphics.drawText(formatted, 350, 50)
+   playdate.graphics.drawTextAligned(formatted, 370, 50, kTextAlignment.right)
+
+   -- TODO: when we have several inputs, move this to the next.
+   -- TODO: have this selection blink and underline the current input perfectly
+   playdate.graphics.drawLine(300, 70, 360, 70)
 
    -- TODO: make the sizing of the various results sized based upon how much is shown.
    --       do we maybe need a few different font sizes for that?
-   playdate.graphics.drawLine(0, 0, 30, 30)
-   playdate.graphics.drawText("Result: ", 5, 150)
+   playdate.graphics.drawText("Result: ", 5, 100)
    if SETTINGS.showDecimal then
       local resultDecimal = string.format("Decimal: %d", currentInput)
-      playdate.graphics.drawText(resultDecimal, 5, 175)
+      playdate.graphics.drawText(resultDecimal, 5, 120)
    end
    if SETTINGS.showHex then
       local resultHex = string.format("Hexadecimal: %x", currentInput)
-      playdate.graphics.drawText(resultHex, 5, 200)
+      playdate.graphics.drawText(resultHex, 5, 140)
    end
    if SETTINGS.showBinary then
       local binaryString = decimalToBinary(currentInput)
-      local resultBin = string.format("Binary: %032s", binaryString)
-      playdate.graphics.drawText(resultBin, 5, 200)
+      local resultBin = string.format("%s", binaryString)
+      playdate.graphics.drawText("Binary:", 5, 160)
+      playdate.graphics.drawText(resultBin, 5, 180)
+   end
+   if SETTINGS.showOctal then
+      local octalString = decimalToOctal(currentInput)
+      local resultOct = string.format("Octal: %s", octalString)
+      playdate.graphics.drawText(resultOct, 5, 200)
    end
 end
